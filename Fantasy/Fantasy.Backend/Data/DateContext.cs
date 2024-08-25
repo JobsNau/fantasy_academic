3480db551bc750b6f2eab2ced36efb65d5c6a737
jnausaca <jobany.nausa@tigo.com.co>
2024-08-24 22:58:19 -0500

    Add countries entity

---
+++ Fantasy/Fantasy.Backend/Data/DateContext.cs
@@ -0,0 +1,20 @@
+﻿using Fantasy.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace Fantasy.Backend.Data;
+
+public class DateContext : DbContext
+{
+    public DateContext(DbContextOptions<DateContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+    }
+}
